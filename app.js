d4da6eb65e99b07da1b11096e46e59f792a77dfe
Yash Sharma <yash2003.ys@gmail.com>
2023-08-14 14:36:37 +0530

    chanced port setting

--- app.js
+++ app.js
@@ -132,8 +132,11 @@ app.post("/delete",function(req,res){
 });
 
 
-
-app.listen(3000,()=>
+let port=process.env.PORT;
+if(port===null||port===""){
+  port="3000"
+}
+app.listen(port,()=>
 {
-  console.log("server is running");
+  console.log("server is running on "+port);
 });
